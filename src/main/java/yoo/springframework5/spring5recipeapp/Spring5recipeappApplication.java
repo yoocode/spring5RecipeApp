5baf4e8189865e98447abd5674230501882ca9ba
Byung-chun Yoo <yoobc1@gmail.com>
2017-12-29 16:10:29 -0500

    first commit

---
+++ src/main/java/yoo/springframework5/spring5recipeapp/Spring5recipeappApplication.java
@@ -0,0 +1,12 @@
+package yoo.springframework5.spring5recipeapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring5recipeappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring5recipeappApplication.class, args);
+	}
+}
