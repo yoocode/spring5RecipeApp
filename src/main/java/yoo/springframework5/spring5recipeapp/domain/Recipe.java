6fc5a3b1cedccb95d3d1c0263a311ed4462cbb92
Byung-chun Yoo <yoobc1@gmail.com>
2017-12-30 23:16:34 -0500

    added ManyToMany bidirectional relationship with consolidation of two tables to one with @JoinTable details

--- src/main/java/yoo/springframework5/spring5recipeapp/domain/Recipe.java
+++ src/main/java/yoo/springframework5/spring5recipeapp/domain/Recipe.java
@@ -3,6 +3,7 @@ package yoo.springframework5.spring5recipeapp.domain;
 import javax.persistence.*;
 import java.util.Set;
 
+@Entity
 public class Recipe {
 
     @Id
@@ -16,8 +17,6 @@ public class Recipe {
     private String source;
     private String url;
     private String directions;
-    //todo add
-    //private Difficulty difficulty;
 
     @OneToOne(cascade = CascadeType.ALL)//if Recipe is deleted, its notes will be deleted as well
     private Notes notes;
@@ -25,6 +24,12 @@ public class Recipe {
     //relationship n a recipe object owns ingredients
     @OneToMany(cascade = CascadeType.ALL, mappedBy = "recipe")
     private Set<Ingredient> ingredients;
+
+    @ManyToMany
+    @JoinTable(name = "recipe_category",
+            joinColumns = @JoinColumn(name = "recipe_id"),
+            inverseJoinColumns = @JoinColumn(name = "category_id"))
+    private Set<Category> categories;
 
     @Lob
     private Byte[] image;
@@ -119,5 +124,21 @@ public class Recipe {
 
     public void setImage(Byte[] image) {
         this.image = image;
+    }
+
+    public Set<Category> getCategories() {
+        return categories;
+    }
+
+    public void setCategories(Set<Category> categories) {
+        this.categories = categories;
+    }
+
+    public Difficulty getDifficulty() {
+        return difficulty;
+    }
+
+    public void setDifficulty(Difficulty difficulty) {
+        this.difficulty = difficulty;
     }
 }
